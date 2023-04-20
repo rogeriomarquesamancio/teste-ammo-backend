3e7590090bd201b7daec83c92c6ec3ad262ba32d
Roger <rogeriomarquesamancio@gmail.com>
2023-04-20 16:59:52 -0300

    Fix errors in database config

--- src/doc/Swagger.ts
+++ src/doc/Swagger.ts
@@ -6,7 +6,7 @@ const doc = {
         title: 'Ammo Varejo RestAPI',
         description: 'Somos responsáveis pelas operações de varejo das marcas Santista, ARTEX, Persono, mmartan e casa moysés!',
     },
-    host: 'ammo-varejo-api.onrender',
+    host: 'ammo-varejo-api.onrender.com',
     schemes: ['https']
 };
 
